83cc209b56f38b7348b0ee2c940a3217a94cd07c
omegadan01 <dbruzzes@nd.edi>
2017-09-25 02:35:27 -0400

    DB finished question 1. Everything Works!

--- exercise5 script.py
+++ exercise5 script.py
@@ -1,10 +1,26 @@
-#question 1
+###########question 1################
 
+#load the dataset
 import pandas
 wages = pandas.read_csv("wages.csv")
-print wages.head (n=5)
 
-#question 2
+#filter out columns of interest into a new dataframe
+wages2 = wages[['gender', 'yearsExperience']].copy()
+
+#drop duplicates from column 1 and 2
+dwage=wages2.drop_duplicates(subset=['gender','yearsExperience'])
+
+#sort columns
+swage= dwage.sort_values(by=["gender", "yearsExperience"])
+
+#check dataframe
+print swage
+
+#write.csv
+swage.to_csv('unique_gender_yearsExperience.csv', sep='\t')
 
 
-#question 3
+####################question 2#################
+
+
+############question 3##############
